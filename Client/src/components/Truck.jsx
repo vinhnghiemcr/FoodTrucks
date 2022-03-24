56abac7c2c1663ce0ddec5185e6aa174fb41f76f
Chinwendu <chinwenduukoha@gmail.com>
2022-03-24 18:13:00 -0400

    corrected errors in truck component

--- Client/src/components/Truck.jsx
+++ Client/src/components/Truck.jsx
@@ -1,55 +1,55 @@
-<<<<<<< HEAD
-
-const Truck = () => {
-<<<<<<< HEAD
-
-    return (
-      
-    )
-=======
->>>>>>> 19e7f98f79df72cbac398ac5db457e79ca368a2d
-}
-
-export default Truck
-=======
-// import { Link } from 'react-router-dom'
-// import Menu from "./Menu"
-// import Review from './Review'
-// import CheckOut from './CheckOut'
-// import {}
+// // HEAD
 
 // const Truck = () => {
+// // HEAD
 
-//   const [items, setItems] = useState([])
-
-//   useEffect(() => {
-//     let isSelected = false
-//     const getTruck = async () => {
-//       const response = await axios.get(
-//         '/food-trucks/:id'
-//       )
-//       if (!isSelected) {
-//         setItems(response.food-trucks)
-//       }
-//     }
-//     const getMenu = async () => {
-//       const response = await axios.get(
-//         '/food-trucks/:id'
-//       )
-//       if (!isSelected) {
-//         setItems(response.food-trucks)
-//       }
-//     }
-//     getItems()
-//     return () => {
-//       isSelected = true
-//     }
-//   })
-
-//   return (
-
-//   )
+//     return (
+      
+//     )
+// =======
+// >>>>>>> 19e7f98f79df72cbac398ac5db457e79ca368a2d
 // }
 
 // export default Truck
->>>>>>> a290984e516ecfbd254aa58203339a3172b2e8d1
+// =======
+// // import { Link } from 'react-router-dom'
+// // import Menu from "./Menu"
+// // import Review from './Review'
+// // import CheckOut from './CheckOut'
+// // import {}
+
+// // const Truck = () => {
+
+// //   const [items, setItems] = useState([])
+
+// //   useEffect(() => {
+// //     let isSelected = false
+// //     const getTruck = async () => {
+// //       const response = await axios.get(
+// //         '/food-trucks/:id'
+// //       )
+// //       if (!isSelected) {
+// //         setItems(response.food-trucks)
+// //       }
+// //     }
+// //     const getMenu = async () => {
+// //       const response = await axios.get(
+// //         '/food-trucks/:id'
+// //       )
+// //       if (!isSelected) {
+// //         setItems(response.food-trucks)
+// //       }
+// //     }
+// //     getItems()
+// //     return () => {
+// //       isSelected = true
+// //     }
+// //   })
+
+// //   return (
+
+// //   )
+// // }
+
+// // export default Truck
+// >>>>>>> a290984e516ecfbd254aa58203339a3172b2e8d1
