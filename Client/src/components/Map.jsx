76507d7930311ddfffdffef9f67780b1c29c87fb
Chinwendu <chinwenduukoha@gmail.com>
2022-03-24 18:08:29 -0400

    homepage map fixed

--- Client/src/components/Map.jsx
+++ Client/src/components/Map.jsx
@@ -1,12 +1,11 @@
 import { ReactDOM } from 'react-router-dom'
 import TruckLocation from './TruckLocation'
-import ThemePark from '../assets/ThemePark.jpeg'
+// import ThemePark from '../assets/ThemePark.jpeg'
 
 const Map = () => {
 
   return (
-    <div className="map">
-      <img src={ThemePark} className="mapContainer" alt="mainHomepage" />
+    <div className="mapContainer">
       <TruckLocation />
     </div>
 
