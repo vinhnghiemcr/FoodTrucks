c58c2a424d0b70627e207028af6be0ee0311c906
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-03-24 11:02:00 -0500

    update Menu.js

--- Server/models/Menu.js
+++ Server/models/Menu.js
@@ -2,9 +2,8 @@ const { Schema } = require('mongoose')
 
 const Menu = new Schema(
     {
-        name: {type: String, required: true},
+        name: {type: String, required: false},
         items: [{ type: Schema.Types.ObjectId,  ref: 'Item', required: true}] ,
-        truck: { type: Schema.Types.ObjectId, ref: 'FoodTruck', required: true }
     },
     { timestamps: true}
 )
