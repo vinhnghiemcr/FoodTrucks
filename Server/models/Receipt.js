f3112eb67da244e8c1e9713fd40a3de39a2480bd
canhvo16 <canhvo16@gmail.com>
2022-03-24 13:35:29 -0500

    added routes and controllers for menu and items

--- Server/models/Receipt.js
+++ Server/models/Receipt.js
@@ -2,13 +2,15 @@ const { Schema } = require('mongoose')
 
 const Product = new Schema(
   {
-    items: [{ type: Schema.Types.ObjectId,  ref: 'Item', required: true}],
+    items: [{ type: Schema.Types.ObjectId, ref: 'Item', required: true }],
     totalCost: { type: Number, required: true },
-    date: { type: Date, required: true },
-    paid: { type: Boolean, required: true},
+    date: { type: String, required: true },
+    paid: { type: Boolean, required: true },
     truck: { type: Schema.Types.ObjectId, ref: 'FoodTruck', required: true }
   },
   { timestamps: true }
 )
 
 module.exports = Product
+
+// 623cb28083a8621966c335ec
