a00d4b90bb032dd6190ba9bafe4bd0d13f72c970
canhvo16 <canhvo16@gmail.com>
2022-03-24 17:55:11 -0500

    added useEffect and useState to homepage

--- Client/src/components/Map.jsx
+++ Client/src/components/Map.jsx
@@ -1,6 +1,7 @@
 import { ReactDOM } from 'react-router-dom'
 import TruckLocation from './TruckLocation'
 import ThemePark from '../assets/ThemePark.jpeg'
+import React from 'react'
 
 const Map = () => {
 
--- Client/src/components/TruckLocation.jsx
+++ Client/src/components/TruckLocation.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Truck from '../assets/foodtruckIcon.png'
 
 const TruckLocation = () => {
