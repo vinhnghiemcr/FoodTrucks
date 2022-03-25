17d3d5c095227c21ee6690579ef5cad6e6d79e8d
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-03-25 08:35:01 -0500

    get the cart feature worked

--- Client/src/components/Item.jsx
+++ Client/src/components/Item.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
 const Item = ({ menuItem, onClick }) => {
+
+  // const handleClick = (item, price) => {
+  //   onclick(item, price)
+  // }
   return (
     <div>
-      <div className="itemName" onClick={onClick}>
+      <div className="itemName" onClick={(e) => onClick(e, menuItem._id)}>
         <h3>{menuItem.name}</h3>
         {/* <h5>{menuItem.description}</h5>
         <h5>{menuItem.image}</h5> */}
         <h5>{menuItem.price}</h5>
+        <button >+</button>
+        <button >-</button>
       </div>
     </div>
   )
