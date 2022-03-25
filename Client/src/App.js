5637e98cd2a711caa0476b1b14066483f5d6ea65
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-03-25 01:54:17 -0500

    update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,9 +2,9 @@ import { Routes, Route } from 'react-router-dom'
 import './style/App.css'
 import Nav from './components/Nav'
 import About from './pages/AboutPage'
-import TruckPage from './pages/TruckPage'
 import HomePage from './pages/HomePage'
 import React from 'react'
+import Truck from './components/Truck'
 
 const App = () => {
   return (
@@ -14,7 +14,7 @@ const App = () => {
         <Routes>
           <Route index element={<HomePage />} />
           <Route path="/about" element={<About />} />
-          <Route path="/food-truck" element={<TruckPage />} />
+          <Route path="/food-trucks/:ftid" element={<Truck />} />
         </Routes>
       </main>
     </div>
