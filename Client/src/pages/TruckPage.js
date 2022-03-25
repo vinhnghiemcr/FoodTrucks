dbd811c7dfdaacb5e504c5ea43a4dffaddf0cf94
Chinwendu <chinwenduukoha@gmail.com>
2022-03-24 21:36:26 -0400

    molly check

--- Client/src/pages/TruckPage.js
+++ Client/src/pages/TruckPage.js
@@ -1,26 +1,20 @@
-const data =
-{ 
-    "name" : "Express Food Truck",
-    "menu" : 
-    { 
-        "name": "Drinks menu",
-        "items": [
-            {
-                "name" : "Coke",
-
-            },
-            {
-                "name" : "Apple",
-
-            },
-            {
-                "name" : "water",
-            }
-        ]
-
-    },
-    "location" : "North West",
-    "image": "",
-    "reviews": ["hkdsjfgak", "dskgfka"],
-
+const data = {
+  name: 'Express Food Truck',
+  menu: {
+    name: 'Drinks menu',
+    items: [
+      {
+        name: 'Coke'
+      },
+      {
+        name: 'Apple'
+      },
+      {
+        name: 'water'
+      }
+    ]
+  },
+  location: 'North West',
+  image: '',
+  reviews: ['hkdsjfgak', 'dskgfka']
 }
