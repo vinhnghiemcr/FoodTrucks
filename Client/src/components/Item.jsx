14daa1d61083aa58fbf84ffe8df8d199fe986d72
Chinwendu <chinwenduukoha@gmail.com>
2022-03-25 13:52:58 -0400

    populated new data

--- Client/src/components/Item.jsx
+++ Client/src/components/Item.jsx
@@ -4,11 +4,11 @@ const Item = ({ menuItem, onClickAdd, onClickMinus }) => {
 
   
   return (
-    <div>
+    <div className='Item'>
       <div className="itemName" >
+        <img src={menuItem.image} alt='food-image-picture'/>
         <h3>{menuItem.name}</h3>
-        {/* <h5>{menuItem.description}</h5>
-        <h5>{menuItem.image}</h5> */}
+        <h5>{menuItem.description}</h5>
         <h5>{menuItem.price}</h5>
         <button onClick={(e) => onClickAdd(e, menuItem._id)}>+</button>
         <button onClick={(e) => onClickMinus(e, menuItem._id)}>-</button>
